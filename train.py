c2d455faf38d4009cad7678fe581a30a978f1003
Micha Krapp <michakrapp@gmail.com>
2026-03-23 00:55:40 +0000

    experiment: double device batch size to 32

--- train.py
+++ train.py
@@ -403,7 +403,7 @@ FINAL_LR_FRAC = 0.0
 
 # Model size
 DEPTH = 4
-DEVICE_BATCH_SIZE = 16
+DEVICE_BATCH_SIZE = 32
 FINAL_EVAL_BATCH_SIZE = get_safe_eval_batch_size(default=256)
 STARTUP_EXCLUDE_STEPS = 1
 
